ac5222e7119be70f0489c8ac7a31fbe101c90dbf
Githubaments <flynnshane@gmail.com>
2020-11-20 20:50:15 +0000

    Update false_positives.py

--- false_positives.py
+++ false_positives.py
@@ -3,6 +3,9 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
+st.set_page_config(page_title='False Positives', page_icon="https://raw.githubusercontent.com/Githubaments/Images/main/favicon.ico")
+
+
 def false_positives():
 
     st.header("False Positives")
